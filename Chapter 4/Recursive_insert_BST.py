ef607ca1b324a09ca714361feeb64d58a8acf2a1
SteviDunn <81661702+SteviDunn@users.noreply.github.com>
2024-11-24 00:37:08 -0800

    Recursive insert

---
+++ Chapter 4/Recursive_insert_BST.py
@@ -0,0 +1,45 @@
+#defining a binary searhc tree 
+#Binary tree: each node at most 2 children
+#Left child must always be less than the parent
+#right child is greater than parent
+
+#Define BST
+#diagram to code
+#top node data --> data in the node, Left child and right child (root)
+class Node: 
+    #need tree constructor for the first node and initilization
+    def __init__(self, data):
+        self.left = None
+        self.right = None
+        self.data = data 
+
+    #need to add a method to add nodes
+    def insert(self, data):
+        #check if existing data is none
+        if self.data is None:
+            self.data = data
+        else:
+            if data < self.data:
+                #go to left sub tree
+                #check if none
+                if self.left is None:
+                    self.left = self.data
+                else:
+                    #need to recursively insert
+                    #the left node is seen as a new
+                    #beginning node of a tree recursive
+                    self.left.insert(data)
+            elif data > self.data:
+                #go to left sub tree
+                #check if none
+                if self.right is None:
+                    self.right = self.data
+                else:
+                    #need to recursively insert
+                    #the left node is seen as a new
+                    #beginning node of a tree recursive
+                    self.right.insert(data)
+##Note
+#In the code provided, the data storage structure
+# for the binary search tree (BST) is implicitly defined by
+# the Node class itself
